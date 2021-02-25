c8d42aef139e3cc862bc49ef827b0913b59a35b4
jingdomaaa <1248135091@qq.com>
2021-02-25 14:44:14 +0800

    deploy

---
+++ js/BaiDu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?edd4238b4eee62e7193e02019bc24761";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
